0d26821ccd60ace8b49b50cf064a93f648ac60c9
morettimarco <44193355+morettimarco@users.noreply.github.com>
2025-02-20 14:29:51 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -7,7 +7,7 @@ const urlsToCache = [
   // './script.js',
   './manifest.json',
   //'./icons/icon-192x192.png'
-   './icons/icon-512x512.png'
+   './icon-512x512.png'
 ];
 
 self.addEventListener('install', (event) => {
